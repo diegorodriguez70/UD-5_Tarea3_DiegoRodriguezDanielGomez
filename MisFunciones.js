2bc85f16f93cdf117ee8cc30e3fa8c2f86db4447
Mañana <you@example.com>
2024-12-12 10:05:23 +0100

    Añadir función cambiarTexto() que cambia el texto de un elemento

--- MisFunciones.js
+++ MisFunciones.js
@@ -4,3 +4,6 @@ function saludar() {
   function cambiarColorFondo() {
     document.body.style.backgroundColor = "lightgreen";
   }
+  function cambiarTexto() {
+    document.getElementById("mensaje").innerText = "Texto cambiado desde JavaScript";
+  }
